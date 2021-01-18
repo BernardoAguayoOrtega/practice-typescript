1fd08f0774e6202fa0ed07a28b5e83a7921856d3
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-18 10:00:01 -0600

    [update] refator inventoryStone class to typescript

--- src/model.ts
+++ src/model.ts
@@ -2,11 +2,16 @@ let displayName: string = "Jess's standing desk";
 let inventoryType: string = 'furniture';
 let trackingNumber: string = 'FD123455';
 let createDate: Date = new Date();
-let originalCost: number = 425;
+let originalCost: number | string = 425;
+
+enum  InventoryItemType {
+	Computer = 'computer',
+	Furniture = 'furniture'
+}
 
 interface InventoryItem {
 	displayName: string;
-	inventoryType: string;
+	inventoryType: 'computer' | 'furniture';
 	readonly trackingNumber: string;
 	createDate: Date;
   originalCost?: number;
@@ -18,7 +23,9 @@ function getInventoryItem(trackingNumber: string): InventoryItem {
 	return null;
 }
 
-function saveInventoryItem(item: InventoryItem) {}
+function saveInventoryItem(item: InventoryItem) {
+
+}
 
 let inventoryItem = getInventoryItem(trackingNumber);
 
